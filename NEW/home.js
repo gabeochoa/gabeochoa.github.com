7b431a55f3d13d82827415b95332daa701f2454a
Gabe <gabe1118@yahoo.com>
2016-06-02 12:04:59 -0400

    home

---
+++ NEW/home.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router";
+
+class Hero extends React.Component {
+
+  static propTypes = {
+    backgroundImage: React.PropTypes.string,
+  };
+
+  render() {
+    const _style = {};
+    if (this.props.backgroundImage) {
+      _style.backgroundImage = 'url(${this.props.backgroundImage})';
+    }
+    return (
+      <div {... this.props} style={_style}>
+        <Container>
+          { this.props.children }
+        </Container>
+      </div>
+    );
+  }
+}
+
+class Page extends React.Component{
+    render(){
+        return(
+        <Hero backgroundImage="img/hero-bg-01.jpg">
+            <h1 className="display-4"> Declarative Landing Pages for React.js </h1>
+            <p className="lead">Build a beautiful landing page in less than an hour.
+                No more redundant code. Easily extensible.</p>
+        </Hero>
+        );
+    }
+};
+
+React.render(<Page/>, document.getElementById("container"));
